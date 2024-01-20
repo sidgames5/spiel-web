f8b0fa519972809c31e23b33c6cbdf7b4ea09933
SidGames5 <saytleader@outlook.com>
2024-01-20 15:03:47 -0500

    Add a please wait message

--- src/Main.hx
+++ src/Main.hx
@@ -1,36 +1,50 @@
 import js.Cookie;
 import js.Browser;
 
+using StringTools;
+
 class Main {
 	static function main() {
 		var doc = Browser.document;
 		var container = doc.getElementById("container");
 
-		// check if a token is stored in the cookies
-		if (Cookie.exists("token")) {
-			// TODO: authenticate with the server
+		if (Browser.window.location.href.contains("handle_login")) {
+			var title = doc.createElement("h1");
+			title.innerHTML = "Please Wait";
+
+			container.appendChild(title);
+		} else if (Browser.window.location.href.contains("handle_register")) {
+			var title = doc.createElement("h1");
+			title.innerHTML = "Please Wait";
+
+			container.appendChild(title);
 		} else {
-			var sbox = doc.createDivElement();
-			sbox.classList.add("loginBox");
+			// check if a token is stored in the cookies
+			if (Cookie.exists("token")) {
+				// TODO: authenticate with the server
+			} else {
+				var sbox = doc.createDivElement();
+				sbox.classList.add("loginBox");
 
-			var lbTitle = doc.createElement("h1");
-			lbTitle.innerHTML = "Welcome to Spiel";
+				var lbTitle = doc.createElement("h1");
+				lbTitle.innerHTML = "Welcome to Spiel";
 
-			var lbButton = doc.createButtonElement();
-			lbButton.innerHTML = "Log in";
-			lbButton.classList.add("loginBoxButton");
-			lbButton.onclick = LoginPage.renderLogin;
+				var lbButton = doc.createButtonElement();
+				lbButton.innerHTML = "Log in";
+				lbButton.classList.add("loginBoxButton");
+				lbButton.onclick = LoginPage.renderLogin;
 
-			var sgButton = doc.createButtonElement();
-			sgButton.innerHTML = "Sign up";
-			sgButton.classList.add("loginBoxButton");
-			sgButton.onclick = LoginPage.renderSignup;
+				var sgButton = doc.createButtonElement();
+				sgButton.innerHTML = "Sign up";
+				sgButton.classList.add("loginBoxButton");
+				sgButton.onclick = LoginPage.renderSignup;
 
-			sbox.appendChild(lbTitle);
-			sbox.appendChild(lbButton);
-			sbox.appendChild(doc.createBRElement());
-			sbox.appendChild(sgButton);
-			container.appendChild(sbox);
+				sbox.appendChild(lbTitle);
+				sbox.appendChild(lbButton);
+				sbox.appendChild(doc.createBRElement());
+				sbox.appendChild(sgButton);
+				container.appendChild(sbox);
+			}
 		}
 	}
 }
