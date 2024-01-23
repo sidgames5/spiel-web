da8aab4ab1817a08195c78bce7b48f7153ae1c27
SidGames5 <saytleader@outlook.com>
2024-01-23 07:16:53 -0500

    Dynamically add css

--- src/AppRenderer.hx
+++ src/AppRenderer.hx
@@ -1,3 +1,4 @@
+import js.Cookie;
 import js.Browser;
 
 class AppRenderer {
@@ -5,5 +6,15 @@ class AppRenderer {
 		var doc = Browser.document;
 		var container = doc.getElementById("container");
 		Utils.wipePage(doc);
+
+		var css = doc.createLinkElement();
+		css.rel = "stylesheet";
+		if (Cookie.exists("theme"))
+			css.href = 'app_${Cookie.get("theme")}.css';
+		else {
+			Cookie.set("theme", "light");
+			css.href = "app_light";
+		}
+		doc.head.appendChild(css);
 	}
 }
